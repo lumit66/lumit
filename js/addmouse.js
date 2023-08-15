6a6baa41bf47b81c0d6b09bf156d6f53e90df774
助学成V <3080853822@qq.com>
2023-08-15 18:33:49 +0800

    Site updated: 2023-08-15 18:33:48

--- js/addmouse.js
+++ js/addmouse.js
@@ -1,16 +1,14 @@
-    // 1.获取元素
-    var oBox = document.getElementById(".aplayer-body");
+window.onload = function() {
+    // 1.获取元素，使用类名选择器
+    var oBox = document.querySelector(".aplayer-body");
 
     // 2.鼠标按下事件
     oBox.onmousedown = function (ev) {
         var ev = ev || window.event;
 
-
         // 获取鼠标相对于盒子的坐标
         var x2 = ev.offsetX;
         var y2 = ev.offsetY;
-
-
 
         // 鼠标移动
         document.onmousemove = function (ev) {
@@ -18,14 +16,12 @@
             var x3 = ev.pageX;
             var y3 = ev.pageY;
             oBox.style.top = y3 - y2 + "px";
-            oBox.style.left = x3 - x2 + "px"
+            oBox.style.left = x3 - x2 + "px";
         }
     }
 
     // 4.鼠标松开事件
     document.onmouseup = function () {
-        document.onmousemove = function () {
-
-        }
+        document.onmousemove = null;
     }
-
+};
