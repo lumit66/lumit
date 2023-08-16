b5281fd89bff852fa81262d5c756b196ecd3e2c2
助学成V <3080853822@qq.com>
2023-08-16 14:26:12 +0800

    Site updated: 2023-08-16 14:26:12

--- js/foot.js
+++ js/foot.js
@@ -24,5 +24,5 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML='<font style=color:#afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+span_dt_dt.innerHTML='<font style=color:#3898e2>'+daysold+'</font> 天 <font style=color:#e96988>'+hrsold+'</font> 时 <font style=color:#e7a624>'+minsold+'</font> 分 <font style=color:#9dd940>'+seconds+'</font> 秒';
 }
