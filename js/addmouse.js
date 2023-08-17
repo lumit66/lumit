bc8628f39eb0832676c8566d558731f1a6e4d585
助学成V <3080853822@qq.com>
2023-08-17 17:18:17 +0800

    Site updated: 2023-08-17 17:18:16

--- js/addmouse.js
+++ js/addmouse.js
@@ -1,9 +1,8 @@
-window.onload = function () {
-  var mouseOffsetX = 0; // 记录当前鼠标位置
+document.addEventListener("DOMContentLoaded", function () {
+  var mouseOffsetX = 0;
   var mouseOffsetY = 0;
-  var isDraging = false; // 记录元素是否可以拖动
+  var isDraging = false;
 
-  // 鼠标事件1：鼠标按下标记元素为可拖动状态，并且记下鼠标当前位置的偏移
   var oBox = document.querySelector('.aplayer-body');
 
   oBox.addEventListener('mousedown', function (e) {
@@ -14,7 +13,6 @@ window.onload = function () {
     isDraging = true;
   });
 
-  // 鼠标事件2：鼠标开始移动，要检测浮层是否标记为移动，如果是则更新元素位置到当前鼠标位置
   document.onmousemove = function (e) {
     var e = e || window.event;
     var moveX = 0;
@@ -29,8 +27,7 @@ window.onload = function () {
     }
   };
 
-  //  鼠标事件3：放开鼠标后，元素变为不可拖动
   document.onmouseup = function () {
     isDraging = false;
   };
-};
+});
