efcfbd51749a3d69597861cbbbe851969025ef47
助学成V <3080853822@qq.com>
2023-08-17 16:38:54 +0800

    Site updated: 2023-08-17 16:38:54

--- js/addmouse.js
+++ js/addmouse.js
@@ -1,36 +1,109 @@
-window.onload = function () {
-  var mouseOffsetX = 0; // 记录当前鼠标位置
+document.addEventListener('DOMContentLoaded', function () {
+  var mouseOffsetX = 0; // 鼠标点击位置相对于元素左上角的偏移量
   var mouseOffsetY = 0;
-  var isDraging = false; // 记录元素是否可以拖动
+  var isDragging = false; // 是否正在拖动元素
 
-  // 鼠标事件1：鼠标按下标记元素为可拖动状态，并且记下鼠标当前位置的偏移
-  var oBox = document.querySelector('.aplayer-body');
+  var oBox = document.querySelector('.aplayer-body'); // 需要拖动的元素
+  var container = document.body; // 容器元素
 
-  oBox.addEventListener('mousedown', function (e) {
-    var e = e || window.event;
-    mouseOffsetX = e.pageX - oBox.offsetLeft;
-    mouseOffsetY = e.pageY - oBox.offsetTop;
+  var startX = 0; // 拖动开始时的鼠标位置
+  var startY = 0;
+  var moveX = 0; // 拖动过程中的鼠标位置
+  var moveY = 0;
+  var deltaX = 0; // 鼠标在x轴和y轴上的位移
+  var deltaY = 0;
+  var lastMoveTime = 0; // 上次移动的时间
+  var velocityX = 0; // x轴和y轴上的速度
+  var velocityY = 0;
 
-    isDraging = true;
-  });
+  container.addEventListener('mousedown', handleStart); // 监听鼠标按下事件
+  container.addEventListener('touchstart', handleStart); // 监听触摸开始事件
+  container.addEventListener('mousemove', handleMove); // 监听鼠标移动事件
+  container.addEventListener('touchmove', handleMove); // 监听触摸移动事件
+  container.addEventListener('mouseup', handleEnd); // 监听鼠标松开事件
+  container.addEventListener('touchend', handleEnd); // 监听触摸结束事件
 
-  // 鼠标事件2：鼠标开始移动，要检测浮层是否标记为移动，如果是则更新元素位置到当前鼠标位置
-  document.onmousemove = function (e) {
-    var e = e || window.event;
-    var moveX = 0;
-    var moveY = 0;
+  function handleStart(e) {
+    e.preventDefault();
 
-    if (isDraging === true) {
-      moveX = e.pageX - mouseOffsetX;
-      moveY = e.pageY - mouseOffsetY;
-
-      oBox.style.left = moveX + "px";
-      oBox.style.top = moveY + "px";
+    if (e.type === 'mousedown') {
+      startX = e.clientX;
+      startY = e.clientY;
+    } else if (e.type === 'touchstart') {
+      startX = e.touches[0].clientX;
+      startY = e.touches[0].clientY;
     }
-  };
 
-  //  鼠标事件3：放开鼠标后，元素变为不可拖动
-  document.onmouseup = function () {
-    isDraging = false;
-  };
-};
+    if (e.target === oBox) {
+      isDragging = true; // 开始拖动元素
+      mouseOffsetX = startX - oBox.offsetLeft;
+      mouseOffsetY = startY - oBox.offsetTop;
+    }
+  }
+
+  function handleMove(e) {
+    e.preventDefault();
+
+    if (isDragging) {
+      if (e.type === 'mousemove') {
+        moveX = e.clientX;
+        moveY = e.clientY;
+      } else if (e.type === 'touchmove') {
+        moveX = e.touches[0].clientX;
+        moveY = e.touches[0].clientY;
+      }
+
+      deltaX = moveX - startX;
+      deltaY = moveY - startY;
+
+      var now = Date.now();
+      var deltaTime = now - lastMoveTime;
+      lastMoveTime = now;
+
+      velocityX = deltaTime > 0 ? deltaX / deltaTime : 0;
+      velocityY = deltaTime > 0 ? deltaY / deltaTime : 0;
+
+      startX = moveX;
+      startY = moveY;
+
+      oBox.style.transform = `translate3d(${moveX - mouseOffsetX}px, ${moveY - mouseOffsetY}px, 0)`;
+      // 更新元素位置
+    }
+  }
+
+  function handleEnd() {
+    isDragging = false; // 停止拖动
+
+    var inertiaDuration = 500; // 惯性滚动持续时间
+    var inertiaDistanceX = velocityX * inertiaDuration; // 惯性滚动距离
+    var inertiaDistanceY = velocityY * inertiaDuration;
+
+    var startTime = Date.now();
+
+    function inertiaScroll() {
+      var currentTime = Date.now();
+      var elapsedTime = currentTime - startTime;
+
+      if (elapsedTime < inertiaDuration) {
+        var distanceX = easeOutCubic(elapsedTime, 0, inertiaDistanceX, inertiaDuration);
+        var distanceY = easeOutCubic(elapsedTime, 0, inertiaDistanceY, inertiaDuration);
+
+        var newX = moveX + distanceX;
+        var newY = moveY + distanceY;
+
+        oBox.style.transform = `translate3d(${newX - mouseOffsetX}px, ${newY - mouseOffsetY}px, 0)`;
+
+        requestAnimationFrame(inertiaScroll);
+        // 使用requestAnimationFrame方法在下一帧中执行惯性滚动
+      }
+    }
+
+    requestAnimationFrame(inertiaScroll);
+    // 使用requestAnimationFrame方法开始惯性滚动
+  }
+
+  // 缓动函数
+  function easeOutCubic(t, b, c, d) {
+    return c * ((t = t / d - 1) * t * t + 1) + b;
+  }
+});
